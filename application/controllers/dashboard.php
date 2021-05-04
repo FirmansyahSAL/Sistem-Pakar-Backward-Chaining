14c1762f191a45f51de0670be8cfadd58dea0c0b
Firmansyah SAL <syah.firman55@gmail.com>
2021-05-04 15:25:13 +0700

    halaman dinamis dashboard

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
+
 class Dashboard extends CI_Controller
 {
     public function index()
---
+++ application/views/back/template/template.php
@@ -0,0 +1,6 @@
+<?php $this->load->view('back/template/meta'); ?>
+<?php $this->load->view('back/template/header'); ?>
+<?php $this->load->view('back/template/sidebar'); ?>
+<?php echo $contents; ?>
+<?php $this->load->view('back/template/footer'); ?>
+<?php $this->load->view('back/template/script'); ?>
