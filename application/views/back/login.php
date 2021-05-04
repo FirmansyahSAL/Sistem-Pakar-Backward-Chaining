4b1b7c7840701da8fafca7c669462142cdc8786e
Firmansyah SAL <syah.firman55@gmail.com>
2021-05-04 22:08:30 +0700

    login dashboard done

--- application/views/back/login.php
+++ application/views/back/login.php
@@ -34,7 +34,7 @@
                 <?= validation_errors() ?>
                 <form action="<?= base_url('auth/proses_login') ?>" method="post">
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email">
+                        <input type="email" name="email" class="form-control" placeholder="Email">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
@@ -42,7 +42,7 @@
                         </div>
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password">
+                        <input type="password" name="password" class="form-control" placeholder="Password">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
