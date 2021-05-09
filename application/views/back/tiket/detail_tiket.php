94468a7fef0afd068f1db77bac6012860b65a942
Firmansyah SAL <syah.firman55@gmail.com>
2021-05-09 19:36:27 +0700

    halaman detail tiket done

--- application/views/back/tiket/detail_tiket.php
+++ application/views/back/tiket/detail_tiket.php
@@ -1,106 +1,95 @@
-<div class="subnavbar">
+<div class="wrapper">
+    <div class="content-wrapper">
+        <section class="content-header">
+        </section>
 
-    <div class="container">
-        <div class="row">
-
-            <div class="span13">
-                <div class="widget ">
-                    <div class="widget-header"> <i class="icon-th-list"></i>
-                        <h3>No Tiket <?= $tiket->no_tiket ?></h3>
-                    </div>
-
-                    <div class="widget-content">
-                        <div class="span11">
-
-                            <h4>
-                                <i class="fas fa-globe"></i> IT Helpdesk
-                                <ul class="nav pull-right">
-                                    <small class="float-right">Date : <?= $tiket->tgl_daftar ?></small>
-                                </ul>
-                            </h4>
-                            <br>
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="callout callout-info">
+                            <h5><i class="fas fa-ticket"></i>No Tiket: <?= $tiket->no_tiket ?></h5>
                         </div>
 
-
-                        <div class="span11">
-                            <div class="span3">
-                                <h8 class="bigstats">
-                                    <div class="stat">
-                                        From
-                                        <strong><?= $tiket->username; ?></strong><br>
-                                        <?= $tiket->divisi ?><br>
-                                        <?= $tiket->jabatan ?><br>
-                                        <?= $tiket->email ?><br>
-                                    </div>
-                                </h8>
+                        <div class="invoice p-3 mb-3">
+                            <!-- title row -->
+                            <div class="row">
+                                <div class="col-12">
+                                    <h4>
+                                        <i class="fas fa-globe"></i>IT Helpdesk
+                                        <small class="float-right">Date : <?= $tiket->tgl_daftar ?></small>
+                                    </h4>
+                                </div>
+                                <!-- /.col -->
                             </div>
+                            <!-- info row -->
+                            <div class="row invoice-info">
+                                <div class="col-sm-4 invoice-col">
+                                    From <br>
+                                    <strong><?= $tiket->username; ?></strong>
+                                    <br>
+                                    Divisi : <?= $tiket->divisi ?><br>
+                                    Jabatan :<?= $tiket->jabatan ?><br>
+                                    Email :<?= $tiket->email ?><br>
+                                </div>
+                                <!-- /.col -->
+                                <div class="col-sm-4 invoice-col">
+                                    To <br>
+                                    <b>Status Tiket</b> : <?php if ($tiket->status_tiket == 0) {
+                                                                echo '<span class="badge badge-warning"> Waiting...</span>';
+                                                            } else if ($tiket->status_tiket == '1') {
+                                                                echo '<span class="badge badge-info"> Response...</span>';
+                                                            } else if ($tiket->status_tiket == '2') {
+                                                                echo '<span class="badge badge-success"> Process...</span>';
+                                                            } else {
+                                                                echo '<span class="badge badge-danger"> solved...</span>';
+                                                            } ?>
+                                    <br>
+                                    <br>
+                                    <b> No Tiket :</b>
+                                    <?= $tiket->no_tiket ?>
+                                    <br>
+                                    <b> Selesai :</b>
 
-                            <div class="span3">
-                                <h7 class="bigstats">
-                                    <div class="stat">
-                                        To
-                                        <b>Status Tiket</b> : <?php if ($tiket->status_tiket == 0) {
-                                                                    echo '<span class="badge badge-warning"> Waiting...</span>';
-                                                                } else if ($tiket->status_tiket == '1') {
-                                                                    echo '<span class="badge badge-info"> Response...</span>';
-                                                                } else if ($tiket->status_tiket == '2') {
-                                                                    echo '<span class="badge badge-success"> Process...</span>';
-                                                                } else {
-                                                                    echo '<span class="badge badge-danger"> solved...</span>';
-                                                                } ?>
-                                        <br>
-                                        <br>
-                                        <h5> No Tiket :
-                                            <?= $tiket->no_tiket ?></h5>
-                                        <p></p>
-                                        <b> Selesai :</b>
-                                        <?php
+                                    <?php
 
-                                        if ($tiket->status_tiket == '3') {
-                                            echo $tiket->waktu_tanggapan;
-                                        } else {
-                                            echo "---";
-                                        }
-                                        ?>
-                                    </div>
-                                </h7>
+                                    if ($tiket->status_tiket == '3') {
+                                        echo $tiket->waktu_tanggapan;
+                                    } else {
+                                        echo "---";
+                                    }
+                                    ?>
+                                </div>
                             </div>
-
-                            <br>
-                            <p>&nbsp;</p>
-                            <br>
 
                             <div class="row">
-
-                                <div class="span 3">
-                                    <p class="lead">Keluhan User / Karyawan</p>
-                                    <input type="text" class="span6 disabled" value="<?= $tiket->judul_tiket ?>" disabled>
+                                <div class="col-6">
+                                    <label for=""> Keluhan User / Karyawan</label>
+                                    <input type="text" value="<?= $tiket->judul_tiket ?>" readonly class="form-control">
+                                    <label for="">Keterangan Lengkap</label>
+                                    <textarea rows="6" readonly class="form-control"> <?= $tiket->deskripsi ?> </textarea>
                                 </div>
-                                <div class="span 3">
-                                    <p class="lead">Keterangan Lengkap</p>
-                                    <input type="text" class="span6 disabled" value="<?= $tiket->judul_tiket ?>" disabled>
+                                <div class="col-6">
+                                    <label for="">Tanggapan Dept IT</label>
+                                    <textarea rows="6" readonly class="form-control"> <?= $tiket->tanggapan ?> </textarea>
                                 </div>
 
-                                <div class="span 3">
-                                    <p class="lead">Tanggapan Dept IT</p>
-                                    <th style="width:50%">
-                                        <input type="text" class="span6 disabled" value="<?= $tiket->judul_tiket ?>" disabled>
-                                    </th>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <label for="">Foto Komplain / keluhan</label>
+                                        <img src="<?= base_url('assets/images/tiket/' . $tiket->gambar_tiket); ?>" width="250px">
+                                        <p class="lead">Foto Komplain / keluhan
+                                            asssalamualaikum
+                                            warrahmatulahi
+                                            wabarakatuh
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <!-- /.invoice -->
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
-            </section>
-            <!-- /.content -->
-        </div>
-
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
-</div>
-</div>
-</div>
-</div>
-</div>
 </div>
